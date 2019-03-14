82f4b9766a773f6a09b92a189df0bc9299620256
Kevin Bracy Knight <kevin.bracyknight@colorado.edu>
2019-03-14 16:10:28 -0600

    update

--- code/master.R
+++ code/master.R
@@ -1,2 +1,8 @@
 ### Master Script for Monarch Costs Project
 ## Created by: Kevin Bracy Knight
+
+#Data import & run the original Thogmartin Scenarios
+source("code/thog.scenarios.R")
+
+#Determine costs for each Thogmartin Scenario
+source("code/cost.loop.3.R")
